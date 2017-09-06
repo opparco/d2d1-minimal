53350bde3c7e8e89673b309cfce9a92ee05807c1
opparco <parco@moriq.com>
2017-09-07 07:07:09 +0900

    Import

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Viewer viewer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.ClientSize = new Size(800, 600);
+            viewer = new Viewer();
+            if (viewer.InitializeGraphics(this))
+                timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            viewer.Update();
+            viewer.Render();
+        }
+    }
+}
---
+++ WindowsFormsApp1/Viewer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+using SharpDX;
+using SharpDX.Direct2D1;
+using SharpDX.DXGI;
+
+namespace WindowsFormsApp1
+{
+    class Viewer : IDisposable
+    {
+        Control control;
+
+        SharpDX.Direct2D1.Factory d2dFactory;
+
+        RenderTarget renderTarget;
+        SolidColorBrush solidColorBrush;
+
+        int CreateDeviceIndependentResources()
+        {
+            d2dFactory = new SharpDX.Direct2D1.Factory();
+
+            return 0;
+        }
+
+        int CreateDeviceResources(ref Size2 size)
+        {
+            if (renderTarget == null)
+            {
+                renderTarget = new WindowRenderTarget(d2dFactory, new RenderTargetProperties(), new HwndRenderTargetProperties { Hwnd = control.Handle, PixelSize = size });
+
+                solidColorBrush = new SolidColorBrush(renderTarget, Color.White);
+            }
+            return 0;
+        }
+
+        int DiscardDeviceResources()
+        {
+            if (solidColorBrush != null)
+            {
+                solidColorBrush.Dispose();
+                solidColorBrush = null;
+            }
+            if (renderTarget != null)
+            {
+                renderTarget.Dispose();
+                renderTarget = null;
+            }
+            return 0;
+        }
+
+        public bool InitializeGraphics(Control control)
+        {
+            this.control = control;
+
+            CreateDeviceIndependentResources();
+
+            return true;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Render()
+        {
+            Size2 size = new Size2(control.ClientSize.Width, control.ClientSize.Height);
+
+            CreateDeviceResources(ref size);
+
+            renderTarget.BeginDraw();
+            renderTarget.Clear(Color.Black);
+            renderTarget.DrawRectangle(new RectangleF(16, 16, 64, 64), solidColorBrush);
+            try
+            {
+                renderTarget.EndDraw();
+            }
+            catch (SharpDXException ex) when ((uint)ex.HResult == 0x8899000C) // D2DERR_RECREATE_TARGET
+            {
+                // device has been lost!
+                DiscardDeviceResources();
+            }
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("viewer Dispose now!");
+
+            solidColorBrush?.Dispose();
+            renderTarget?.Dispose();
+
+            d2dFactory?.Dispose();
+        }
+    }
+}
